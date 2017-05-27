b0b3f3042c4fa5704ff3bb671f971cb5bc41d3e5
igimonchik <tipter.nt.1@gmail.com>
2017-05-27 18:02:33 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>123</title>
+  </head>
+  <body>
+      <script type="text/javascript">
+        aler('Hello world');
+      </script>
+  </body>
+</html>
